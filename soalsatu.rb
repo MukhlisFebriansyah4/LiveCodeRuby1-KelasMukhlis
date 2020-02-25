526cd91424aee6a0c8c5b17a2ceb24070301dfd3
YOUR NAME <YOUR@EMAIL.com>
2020-02-25 13:46:27 +0700

    initial commit

---
+++ soalsatu.rb
@@ -0,0 +1,12 @@
+lemari = ["Buku","Handphone","Pensil","Baju","Flashdrive"]
+tas = []
+
+puts "===========Lemari==========="
+puts lemari[1]
+puts lemari[3]
+
+tas = lemari.push
+tas.delete_at(1)
+tas.delete_at(2)
+puts "===========Tas==========="
+puts tas
