1c361f43ab758540f7a2853b95cbbb2ca15bd453
agnesangelasf <agnesangela1702@gmail.com>
2020-02-25 13:39:10 +0700

    Live Code

---
+++ soal3.rb
@@ -0,0 +1,8 @@
+buah = {apel: 5000, nanas: 10000, semangka: 20000, jeruk: 4000, anggur: 6000}
+namabuah = gets.chomp
+jumlah = gets.to_i
+puts "buah yang dibeli :"
+puts "#{jumlah} #{namabuah}"
+
+
+
