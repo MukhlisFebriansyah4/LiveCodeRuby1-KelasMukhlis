9a6a557af33162b6d4ec755b33e6eb46ea601cfb
astridndamayanti <novikaastrid@gmail.com>
2020-02-26 06:25:40 +0700

    Latihan LiveCode

---
+++ soal2.rb
@@ -0,0 +1,12 @@
+array1=[1,2,3,4,5,6,7,8,9]
+array2=[1,7,6,0]
+jumlahRotasi=3
+jumlahRotasi.times do
+array1.unshift(array1.last)
+array2.unshift(array2.last)
+array1.pop
+array2.pop
+end
+puts array1
+puts "========="
+puts array2
